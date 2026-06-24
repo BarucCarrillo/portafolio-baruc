0e7e09ae6d88198ac64519765fa4e78024bb2d70
BarucCarrillo <baruccarrillo@outlook.com>
2026-06-23 21:48:55 -0600

    tools data

---
+++ src/data/tools-data.tsx
@@ -0,0 +1,50 @@
+// 1. Entornos de Desarrollo
+const develop_tools = [
+    { logo: '', title: 'VS Code' },
+    { logo: '', title: 'Sublime Text' },
+    { logo: '', title: 'Claude Code' },
+    { logo: '', title: 'Open Code' },
+];
+
+// 2. Gestión de Base de Datos y ORMs
+const database_tools = [
+    { logo: '', title: 'MongoDB' },
+    { logo: '', title: 'Firebase' },
+    { logo: '', title: 'MySQL' },
+    { logo: '', title: 'SQL Server' },
+    { logo: '', title: 'Prisma Studio' },
+];
+
+// 3. Frameworks y Librerías (Web)
+const web_frameworks = [
+    { logo: '', title: '.NET' },
+    { logo: '', title: 'Next.js' },
+    { logo: '', title: 'Laravel' },
+    { logo: '', title: 'React' },
+];
+
+// 4. Desarrollo Móvil
+const mobile_tools = [
+    { logo: '', title: 'React Native' },
+    { logo: '', title: 'Xamarin' },
+    { logo: '', title: 'Flutter' },
+];
+
+// 5. Herramientas Generales, Infraestructura y Diseño
+const general_tools = [
+    { logo: '', title: 'Postman' },
+    { logo: '', title: 'Git' },
+    { logo: '', title: 'GitHub' },
+    { logo: '', title: 'OWASP ZAP' },
+    { logo: '', title: 'Figma' },
+    { logo: '', title: 'Hetzner' },
+    { logo: '', title: 'Cloudflare' },
+];
+
+export const skills_data = {
+    develop: develop_tools,
+    databases: database_tools,
+    frameworks: web_frameworks,
+    mobile: mobile_tools,
+    general: general_tools
+};
